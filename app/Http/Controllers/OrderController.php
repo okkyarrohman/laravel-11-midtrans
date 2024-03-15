df423681c9693e8bced776dbdc0d5d23e20a9075
okkyarrohman <okkyzee@gmail.com>
2024-03-15 10:22:28 +0700

    commits checkout

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+
+        return view('home');
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->request->add([
+            'total_price' => $request->quantity * 50000,
+            'status' => 'Unpaid',
+        ]);
+
+        $order = Order::create([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'quantity' => $request->input('quantity'),
+            'address' => $request->input('address'),
+        ]);
+
+        //SAMPLE REQUEST START HERE
+
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $order->id,
+                'gross_amount' => $order->total_price,
+            ),
+            'customer_details' => array(
+                'name' => $request->name,
+                'phone' => $request->phone,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+
+        return view('')
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/order', [OrderController::class, 'index']);
+Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
